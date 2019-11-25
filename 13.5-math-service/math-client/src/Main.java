af8c3bc1c3dac8bcdf4d8602e82d8d97a33c10df
yangxvlin <1768528843@qq.com>
2019-11-25 21:41:33 +1100

    add comment

--- 13.5-math-service/math-client/src/Main.java
+++ 13.5-math-service/math-client/src/Main.java
@@ -18,18 +18,24 @@ public class Main {
 
         Socket tcpClient = null;
         try {
+            // prepare
             System.out.println("Connecting to " + serverName + ":" + port1);
             tcpClient = new Socket(serverName, port1);
             System.out.println("Connected to " + serverName + ":" + port1);
 
+            // request
             BufferedWriter writer = new BufferedWriter(
                     new OutputStreamWriter(tcpClient.getOutputStream()));
             writer.write("+:12:21");
             writer.newLine();
             writer.flush();
+
+            // receive
             // get the result from the server
             BufferedReader reader = new BufferedReader(new InputStreamReader(tcpClient.getInputStream()));
             System.out.println("TCP: " + reader.readLine());
+
+            // end
             reader.close();
             writer.close();
             tcpClient.close();
@@ -39,26 +45,26 @@ public class Main {
 
         DatagramSocket udpClient = null;
         try {
+            // prepare
             InetAddress host = InetAddress.getByName(args[0]);
             System.out.println("Connecting to " + serverName + ":" + port2);
             udpClient = new DatagramSocket();
             System.out.println("Connected to " + serverName + ":" + port2);
 
+            // request
             String message = "*:12:21";
             byte[] requestMessage = message.getBytes();
-
             DatagramPacket request = new DatagramPacket(requestMessage, message.length(), host, port2);
 
             udpClient.send(request);
 
+            // receive
             byte[] receiveBuffer = new byte[1024];
-
             DatagramPacket response = new DatagramPacket(receiveBuffer, receiveBuffer.length);
-
             udpClient.receive(response);
-
             System.out.println("UDP: " + new String(response.getData(), 0, response.getLength()));
 
+            // end
             udpClient.close();
         } catch (IOException e) {
             e.printStackTrace();
